f5dab0b7c786e12b9b326c28c9862215f88b113d
dev-rah <69502846+dev-rah@users.noreply.github.com>
2023-11-27 13:53:57 +0100

    Update page.tsx
    Tilføjet Rasmus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ function Niko() {
   return <div className="text-niko">Brok</div>
 }
 
+function Rasmus() {
+  return <div className="text-niko">Rasmus</div>
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       Hej FBG.
-
+      <Rasmus />
       <Niko />
       <Niko />
       <Niko />
